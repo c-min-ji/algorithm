13606ebb3bc7c6eb38e00255d31f71300e0c7004
minji choi <mjeewh@gmail.com>
2021-09-24 10:09:04 +0900

    [UPDATE] Greedy/exchange.py

--- Greedy/README.md
+++ Greedy/README.md
@@ -1,2 +1,4 @@
 # Greedy(탐욕법)
 
+>[거스름돈](./exchange.py)
+단점: 
---
+++ Greedy/exchange.py
@@ -0,0 +1,13 @@
+def solution():
+    n = 1260 # 거슬러줄돈
+    count = 0 # 동전 갯수
+
+    coin_type = [500, 100, 50, 10] # 동전 종류
+
+    for coin in coin_type: # coin_type별로 for문 돔
+        count += n//coin # 동전 갯수에 동전 종류와 거스름돈을 나눈 몫을 더해줌
+        n %= coin # n은 이제 위에서 몫으로 나온 돈을 빼고 남은 돈이 됨
+    
+    return count # 몇 개의 동전을 받았는 지 return함
+    
+print(solution()) # 출력
