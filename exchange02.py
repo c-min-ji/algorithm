c07f940ea991b60b13bf0a6b7014bb8f5800f95f
minji choi <mjeewh@gmail.com>
2021-10-04 21:04:11 +0900

    [UPDATE] exchange02.py comment

--- exchange02.py
+++ exchange02.py
@@ -1,3 +1,5 @@
+# 좀 더 봅시다~ 이게 아마 규칙을 파악해서 쓰는 식인데 count부터 봅시당
+
 def solution():
     n, m, k = map(int, input().split()) # n=num_list 개수, m=몇 번 더하는지, k=같은 수를 최대 몇 번 더할수있는지 input_ex) n, m, k = 5, 8, 3
     num_list = list(input().split()) # num_list=주어지는 수 input_ex) num_list = [2,4,5,4,6]
@@ -7,7 +9,7 @@ def solution():
     first_bNum = num_list[n-1] # 정렬 후 제일 큰 수를 찾음(정렬했으니까 제일 큰 수는 맨 뒤)
     second_bNum = num_list[n-2] # 정렬 후 두 번째 큰 수를 찾음(정렬했으니까 두 번째 큰 수는 맨 뒤에서 하나 앞)
 
-    count = int(m/(k+1))*k
+    count = int(m/(k+1))*k # 몫..?
     count += m%(k+1)
     
     result = 0
