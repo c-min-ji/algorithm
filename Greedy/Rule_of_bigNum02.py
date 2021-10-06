5c9ad45290494bfc66d0f1f066e7f6b27219c49a
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2021-10-06 11:46:58 +0900

    [UPDATE] Rule_of_bigNum02.py
    주석 수정

--- Greedy/Rule_of_bigNum02.py
+++ Greedy/Rule_of_bigNum02.py
@@ -9,8 +9,8 @@ def solution():
     first_bNum = num_list[n-1] # 정렬 후 제일 큰 수를 찾음(정렬했으니까 제일 큰 수는 맨 뒤)
     second_bNum = num_list[n-2] # 정렬 후 두 번째 큰 수를 찾음(정렬했으니까 두 번째 큰 수는 맨 뒤에서 하나 앞)
 
-    count = int(m/(k+1))*k # 몫..?
-    count += m%(k+1)
+    count = int(m/(k+1))*k # 같은 수를 반복해서 더할 수 있는 횟수 k 에 그 다음 작은 수를 더하니까 +1을 해준다. 그리고 총 더하는 횟수인 m이랑 나눠서 몫을 구한다. 여기에 k를 곱한다.. ?
+    count += m%(k+1) # count에 나머지를 더한다..?아 나눠떨어지지않는 경우의 수다...
     
     result = 0
     result += (count)*first_bNum
