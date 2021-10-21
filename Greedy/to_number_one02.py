121f3d9ac6a602eea4f25e1236e69c83dee9e5ed
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2021-10-21 09:15:44 +0900

    [UPDATE] to_number_one02.py
    주석 및 틀린 수식 수정

--- Greedy/to_number_one02.py
+++ Greedy/to_number_one02.py
@@ -2,16 +2,16 @@ def solution():
     n, k = map(int, input().split())
     result = 0
 
-    while True:
-        target = (n//k)*k
-        result += (n-target)
+    while True: #무한반복
+        target = (n//k)*k #몫에 k곱하기
+        result += (n-target) #n에서 target뺀거를 result에 더하기
         n = target
 
-        if n<k:
+        if n<k: #위 과정을 거쳐서 k가 n보다 크면 while문 종료
             break
 
         result += 1
-        n//k
+        n//=k
 
     result += (n-1)
     return result
