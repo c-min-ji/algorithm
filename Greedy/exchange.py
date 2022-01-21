b33884121f4696545950348faeb0c9cdee578466
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-01-21 19:35:24 +0900

    [UPDATE] Greedy/exchange.py

--- Greedy/exchange.py
+++ Greedy/exchange.py
@@ -2,10 +2,10 @@ def solution():
     n = 1260 # 거슬러줄돈
     count = 0 # 동전 개수
 
-    coin_type = [500, 100, 50, 10] # 동전 종류
+    coin_type = [500, 100, 50, 10] # 동전 종류 넣어주기
 
     for coin in coin_type: # coin_type별로 for문 돔
-        count += n//coin # 동전 개수에 동전 종류와 거스름돈을 나눈 몫을 더해줌
+        count += n//coin # 동전 개수에 동전 종류와 거스름돈을 나눈 몫을 더해줌  n/coin도 가능
         n %= coin # n은 이제 위에서 몫으로 나온 돈을 빼고 남은 돈이 됨
     
     return count # 몇 개의 동전을 받았는 지 return함
