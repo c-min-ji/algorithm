c26ca8c5e10661167faf96b27bff14ca581c775d
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-01-21 21:40:06 +0900

    [UPDATE] Greedy/to_number_one02.py

--- Greedy/to_number_one02.py
+++ Greedy/to_number_one02.py
@@ -1,3 +1,4 @@
+#n 값이 클 떄 유리
 def solution():
     n, k = map(int, input().split())
     result = 0
@@ -5,7 +6,7 @@ def solution():
     while True: #무한반복
         target = (n//k)*k #몫에 k곱하기
         result += (n-target) #n에서 target뺀거를 result에 더하기
-        n = target
+        n = target #n을 계속 업데이트함
 
         if n<k: #위 과정을 거쳐서 k가 n보다 크면 while문 종료
             break
