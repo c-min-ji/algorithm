b4f93317b740b02b5d573a8136d1954a1e722060
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-01-21 19:33:07 +0900

    [UPDATE] Greedy/Number_Card_Game02.py

--- Greedy/Number_Card_Game02.py
+++ Greedy/Number_Card_Game02.py
@@ -1,5 +1,4 @@
-# 2중 for문 사용
-
+#이중 for 사용
 def solution():
     N, M = map(int, input().split())
 
@@ -8,10 +7,10 @@ def solution():
     for i in range(N):
         data = list(map(int, input().split()))
 
-        min_num = 10001 #최대 입력 가능 수가 10000이니까 100001을 최소값으로 잡고 시작
+        min_num = 10001 #최대 입력 가능 수가 10000이니까 100001을 최소값으로 잡고 시작 *제약 잘 보기
 
-        for j in data: # for j in range(data)-->안됨....주의! 그냥 in data임
-            min_num = min(min_num, j) # 이 중에 가장 작은 거 우선 고르기
+        for j in data: # for j in range(data)-->안됨....주의! 그냥 in data임 해도 되긴하는데 밑에서 data[i]이렇게 적어줘야함
+            min_num = min(min_num, j) # 이 중에 가장 작은 거 우선 고르기 
 
         result = max(min_num, result) # 그리고 그 중에서 최대값 찾기
     
