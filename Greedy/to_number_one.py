2750c286c20ced7dd8e1c68cca43fbcf7fc1170c
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-01-21 19:37:31 +0900

    [UPDATE] Greedy/to_number_one.py

--- Greedy/to_number_one.py
+++ Greedy/to_number_one.py
@@ -3,11 +3,11 @@ def solution():
     count = 0
 
     while N>=K:
-        while N%K !=0:
+        while N%K !=0: #N이 K로 나누어 떨어질 때까지 -1해줌
             N -= 1
             count += 1
         
-        N //= K
+        N //= K #나누어지면 나눔
         count += 1
     
     while N>1:
