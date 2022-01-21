049e6ab9739df72b556888fccb35d4e86ea02340
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-01-21 19:34:25 +0900

    [UPDATE] Greedy/Rule_of_bigNum.py

--- Greedy/Rule_of_bigNum.py
+++ Greedy/Rule_of_bigNum.py
@@ -2,7 +2,7 @@ def solution():
     n, m, k = map(int, input().split()) # n=num_list 개수, m=몇 번 더하는지, k=같은 수를 최대 몇 번 더할수있는지 input_ex) n, m, k = 5, 8, 3
     num_list = list(input().split()) # num_list=주어지는 수 input_ex) num_list = [2,4,5,4,6]
     
-    num_list.sort() # 오름차순으로 정렬
+    num_list.sort() # 오름차순으로 정렬 내림차순하려면 reverse=True
 
     first_bNum = num_list[n-1] # 정렬 후 제일 큰 수를 찾음(정렬했으니까 제일 큰 수는 맨 뒤)
     second_bNum = num_list[n-2] # 정렬 후 두 번째 큰 수를 찾음(정렬했으니까 두 번째 큰 수는 맨 뒤에서 하나 앞)
