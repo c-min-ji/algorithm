0dbe2eddf322c31ae4a56169c618c2ae972165b7
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-01-24 13:53:08 +0900

    [UPDATE] up_down_left_right.py

--- Implementation/up_down_left_right.py
+++ Implementation/up_down_left_right.py
@@ -8,4 +8,11 @@ move = ['L', 'R', 'U', 'D'] // 방향 단어 넣어주기
 
 for plan in plans:
   for i in range(len(move)):
-    
+    if plan == move[i]:
+      nx = x + dx[i]
+      ny = y + dy[i]
+  if nx < 1 or ny < 1 or nx > n or ny > n:
+    continue
+  x, y = nx, ny
+  
+print(x, y)
