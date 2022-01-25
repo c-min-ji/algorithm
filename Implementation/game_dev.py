08ce6e0ca071366a91f9f2ff1cf6882f694e5c93
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-01-25 14:40:01 +0900

    [UPDATE] Implementation/game_dev.py

--- Implementation/game_dev.py
+++ Implementation/game_dev.py
@@ -1,1 +1,48 @@
+n, m = map(int, input().split())
 
+d = [[0]* m for _ in range(n)]
+
+x, y, direct = map(int, input().split())
+d[x][y] = 1
+
+array = []
+for i in range(n):
+  array.append(list(map(int, input().split())))
+  
+dx = [-1, 0, 1, 0]  #북동남서(0123)
+dy = [0, 1, 0, -1]
+
+def turn_left():
+  global direct
+  direct -= 1
+  if direct == -1:
+    direct = 3
+
+cnt = 1 #처음 시작 카운트
+turn = 0 #회전
+
+while True:
+  turn_left()
+  nx=x+dx[direct]
+  ny=y+dy[direct]
+  
+  if d[nx][ny] === 0 and array[nx][ny] == 0:
+    n[nx][ny] = 1
+    x = nx
+    y = ny
+    cnt += 1
+    continue
+  else:
+    turn +=1
+  if turn == 4:
+    nx = x - dx[direct]
+    ny = y - dy[direct]
+    if array[nx][ny] == 0:
+      x = nx
+      y = ny
+    else:
+      break
+    turn = 0
+
+print(cnt)
+  
