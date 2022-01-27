4f3609fb788dcab2f12aec4f9552753891493ae8
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-01-27 15:55:15 +0900

    [UPDATE] DFS_BFS/maze.py

--- DFS_BFS/maze.py
+++ DFS_BFS/maze.py
@@ -5,6 +5,28 @@ n, m = map(int, input().split())
 graph = []
 for i in range(n):
   graph.append(list(map(int, input())))
-  
-  
+    
 dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x,y):
+  que = deque()
+  que.append((x,y))
+  
+  while que: #que가 빌 때까지
+    x, y = que.popleft()
+    for i in range(4): #4방향 움직일 거라 4
+      nx = x+dx[i]
+      ny = y+dy[i]
+      
+      if nx<0 or ny<0 or nx>=n or nx>=m: #범위 벗어날때
+        continue
+      if graph[nx][ny] == 0: #벽일때
+        continue
+      if graph[nx][ny] == 1: #길일때
+        graph[nx][ny] = graph[x][y]+1
+        que.append((nx,ny))
+  return graph[n-1][m-1]
+
+print(bfs(0,0))
+      
