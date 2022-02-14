0907602beeefe62b0b0ba1e5abe2444b7f9da2a2
c-min-ji <59816464+c-min-ji@users.noreply.github.com>
2022-02-14 11:53:49 +0900

    Create parenthesis.py

---
+++ Example_Problem/parenthesis.py
@@ -0,0 +1,43 @@
+def bal(p):
+    cnt = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt == 0:
+            return i
+
+def check(p):
+    cnt = 0
+    for i in p:
+        if i == '(':
+            cnt += 1
+        else:
+            if cnt == 0:
+                return False
+            cnt -= 1
+    return True
+
+def solution(p):
+    answer = ''
+    if p == "":
+        return answer
+
+    index = bal(p)
+    u = p[:index+1]
+    v = p[index+1:]
+    if check(u):
+        answer = u + solution(v)
+    else:
+        answer='('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += "".join(u)
+    return answer
